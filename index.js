6861ff91914bad478a31f78c5bd9a0a83822e762
Ozodbek Ozodov <o.ozodov@uzinfocom.uz>
2025-03-19 16:03:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
